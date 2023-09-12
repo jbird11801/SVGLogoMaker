ae1036fae60ddf4f72f96f90b5cf37354677d058
jbird11801@gmail.com <jbird11801@gmail.com>
2023-09-11 23:41:33 -0400

    feature/writefile

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # SVGLogoMaker
+
+Protected by a ![Static Badge](https://img.shields.io/badge/MIT-b06402) [License](#license)
+
+## Description
+
+This app is used to make simple svg logos and was all around simple. I mainly wanted to learn about the testing proceudure and sytax here. I had a lot of issues with it. Im supper hapy with how it came out and am eager to learn more about the testing procedure>
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation
+
+prerequisite have node (https://nodejs.org/en) and and command line interface 
+
+1) install this applicaion onto the selected area of a your computer and use the command line and get into the svg logo maker file
+
+2) run the (npm init) command in the command line 
+
+3) run the (npm i inquirer)
+
+## Usage
+
+This app is used to make simple svg logos
+
+## Credits
+
+Used to make read me https://github.com/jbird11801/ReadMeGenerator
+
+Used to make badge https://shields.io/
+
+Used to run tests https://www.npmjs.com/package/jest
+
+Used to ask questions https://www.npmjs.com/package/inquirer
+
+Used to learn about svgs https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes
+
+Used to help make svgs https://marketplace.visualstudio.com/items?itemName=jock.svg
+
+## License
+
+This project has the (MIT) license protecting it!
+
+Please refer to license file 
+
+## Tests
+
+I used npm jest to test my project 
+
+![Immage showing passing results for my test](lib/Images/TestPass.PNG)
+
+## Questions
+
+Please contact me about this project here [jbird11801@yahoo.com](mailto:jbird11801@yahoo.com)
+            
+My git hub account is jbird11801 at https://github.com/jbird11801
--- index.js
+++ index.js
@@ -1,14 +1,122 @@
+
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+
 const Square = require('./lib/Square');
+
+const Triangle = require('./lib/Triangle');
+
+const Circle = require('./lib/Circle');
+
+var indexQuestion = 0;
 
 const data = {
 
-    color : "green",
+  color : "",
+  
+  text : ""
 
-    text : "Aaaa"
+}
 
-};
 
-const square = new Square(data);
 
-console.log(square)
+const QuestionObject = [
 
+   
+      
+      {
+  
+        type: 'rawlist',
+  
+        message: 'What shape do you want for your logo',
+  
+        choices: ["Square","Triangle","Circle"],
+  
+        name: "answer"
+  
+      }
+      ,
+
+      {
+
+        type: 'input',
+  
+        message: 'What color do yoy want that shape to be ',
+  
+        name: "answer"
+  
+      }
+            
+      ,
+      
+      {
+  
+        type: 'input',
+  
+        message: 'What are the letters you want on the logo (less than or equal to 3)',
+  
+        name: "answer"
+  
+      }
+
+]
+
+ShapeFile = function(shape){
+
+  data.text = answers[2];
+
+  data.color = answers[1];
+
+  if (shape === "Square"){
+
+  return  new Square(data);
+
+
+  }
+
+  else if (shape === "Triangle"){
+
+    return new Triangle(data);
+  
+    }
+
+  else {
+
+    return new Circle(data);;
+
+  }
+
+}
+
+const answers = ["","",""];
+
+function ask() {
+
+    if ( indexQuestion < QuestionObject.length){
+  
+      inquirer.prompt(QuestionObject[indexQuestion]).then ((response) => {
+  
+        answers[indexQuestion] = response.answer;
+  
+      indexQuestion++;
+  
+      ask();
+  
+    })
+  
+    }
+  
+    else {
+  
+      fs.writeFile(`logo.svg`, ShapeFile(answers[0]).svgText() , (err) =>
+  
+      err ? console.error(err) : console.log('Generated logo.svg')
+  
+      )
+      
+    }
+
+}
+
+ask();
